6f1428122d41f0becf140c0a8c6af7e579cb14bb
Denny Nuwen <Dennis@flying-sword.com>
2019-07-19 22:03:26 -0700

    Update x3hitsu.js

--- x3hitsu.js
+++ x3hitsu.js
@@ -1,4 +1,2 @@
-document.title("This should work";)
-//document.write("<iframe id="disp" src="about:blank" frameborder="0"></iframe>");
-
-document.write("<h1> Testing 1 2 3 </h1>");
+document.title("x3";)
+document.write("<iframe id="disp" src="//dennynuwen.github.io/chatango.io/x3hitsu.html" frameborder="0"></iframe>");
